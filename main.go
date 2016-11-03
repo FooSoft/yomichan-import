a6ee0cfae21abaf43b6dcfaca681739da1518b8b
Alex Yatskov <alex@foosoft.net>
2016-11-02 22:31:31 -0700

    Add stub for meta generator

--- main.go
+++ main.go
@@ -44,6 +44,7 @@ func usage() {
 
 func outputJson(fileFormat, inputPath, outputDir string, flags int) error {
 	handlers := map[string]func(string, io.Reader, int) error{
+		"meta":     outputMetaJson,
 		"edict":    outputEdictJson,
 		"enamdict": outputJmnedictJson,
 		"kanjidic": outputKanjidicJson,
