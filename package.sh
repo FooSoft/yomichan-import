131d77b64d3c2657cb48cccc4427474407ead2ea
Alex Yatskov <alex@foosoft.net>
2017-01-29 22:26:57 -0800

    setup for deploy with osx

--- package.sh
+++ package.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-gox -os="linux windows" -arch="386"
+gox -os="linux windows darwin" -arch="386"
 
 mkdir -p yomichan-import_windows/bin/windows
 mv yomichan-import_windows_386.exe yomichan-import_windows/yomichan-import.exe
@@ -13,3 +13,9 @@ mv yomichan-import_linux_386 yomichan-import_linux/yomichan-import
 cp bin/linux/* yomichan-import_linux/bin/linux/
 tar czvf yomichan-import_linux.tar.gz yomichan-import_linux
 rm -rf yomichan-import_linux
+
+mkdir -p yomichan-import_darwin/bin/darwin
+mv yomichan-import_darwin_386 yomichan-import_darwin/yomichan-import
+cp bin/darwin/* yomichan-import_darwin/bin/darwin/
+tar czvf yomichan-import_darwin.tar.gz yomichan-import_darwin
+rm -rf yomichan-import_darwin
