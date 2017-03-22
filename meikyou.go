ab67b1f4fd4855ddcef18068749d6de2fec76ea2
Alex Yatskov <alex@foosoft.net>
2017-03-21 20:44:02 -0700

    updating authors

--- meikyou.go
+++ meikyou.go
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 2017 Alex Yatskov <alex@foosoft.net>, flamerokz
- * Author: flamerokz
+ * Copyright (c) 2017 Alex Yatskov <alex@foosoft.net>, ajyliew
+ * Author: ajyliew
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
--- wadai.go
+++ wadai.go
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 2017 Alex Yatskov <alex@foosoft.net>, flamerokz
- * Author: Alex Yatskov <alex@foosoft.net>, flamerokz
+ * Copyright (c) 2017 Alex Yatskov <alex@foosoft.net>, ajyliew
+ * Author: Alex Yatskov <alex@foosoft.net>, ajyliew
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
