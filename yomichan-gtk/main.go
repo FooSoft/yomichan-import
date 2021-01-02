16d75c71e247d7dbd0c5fe1872294e5423d0b40f
Alex Yatskov <alex@foosoft.net>
2021-01-01 16:28:06 -0800

    Update copyright

--- yomichan-gtk/main.go
+++ yomichan-gtk/main.go
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017-2019 Alex Yatskov <alex@foosoft.net>
+ * Copyright (c) 2017-2021 Alex Yatskov <alex@foosoft.net>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
