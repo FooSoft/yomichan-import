83e3e44f46e344bfe66d9c7181caa5b113f8fb2a
Alex Yatskov <alex@foosoft.net>
2021-01-10 17:38:03 -0800

    Update build scripts

--- scripts/build_linux.sh
+++ scripts/build_linux.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+mkdir -p dst
+mkdir -p yomichan-import
+
 go build github.com/FooSoft/yomichan-import/yomichan
 go build github.com/FooSoft/yomichan-import/yomichan-gtk
-
-mkdir yomichan-import
 
 mv yomichan yomichan-import
 mv yomichan-gtk yomichan-import
 
-7za a yomichan-import_linux.7z yomichan-import
+tar czvf dst/yomichan-import_linux.tar.gz yomichan-import
 
 rm -rf yomichan-import
--- scripts/build_windows.sh
+++ scripts/build_windows.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
+mkdir -p dst
+mkdir -p yomichan-import
+
 export CXX=x86_64-w64-mingw32-g++.exe
 export CC=x86_64-w64-mingw32-gcc.exe
-
 go build github.com/FooSoft/yomichan-import/yomichan
 go build -ldflags="-H windowsgui" github.com/FooSoft/yomichan-import/yomichan-gtk
-
-mkdir yomichan-import
 
 mv yomichan.exe yomichan-import
 mv yomichan-gtk.exe yomichan-import
 
-7za a yomichan-import_windows.7z yomichan-import
+7za a yomichan-import_windows.zip yomichan-import
 
 rm -rf yomichan-import
